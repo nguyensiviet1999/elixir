ed30863379c64117d98a2eb5b466e7086a22985d
Viet Elixir <nguyensiviet1999@gmail.com>
2021-03-17 10:47:33 +0700

    supervisor

--- lib/kv.ex
+++ lib/kv.ex
@@ -2,7 +2,14 @@ defmodule KV do
   @moduledoc """
   Documentation for `KV`.
   """
+  use Application
 
+  @impl true
+  def start(_type, _args) do
+    # Although we don't use the supervisor name below directly,
+    # it can be useful when debugging or introspecting the system.
+    KV.Supervisor.start_link(name: KV.Supervisor)
+  end
   @doc """
   Hello world.
 
